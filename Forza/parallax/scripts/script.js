17e34ea88dfa02a000dd639e35a61d3063af52cc
Maksym Vasyukov <maksym.vasyukov@seracom.de>
2017-10-06 16:17:38 +0200

    added stats chaged banners

--- Forza/parallax/scripts/script.js
+++ Forza/parallax/scripts/script.js
@@ -26,3 +26,22 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+// Tabs controller
+
+function openField(evt, target) {
+  var i;
+  var x = document.getElementsByClassName("stat");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  var activebtn = document.getElementsByClassName("testbtn");
+  for (i = 0; i < x.length; i++) {
+      activebtn[i].className = activebtn[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(target).style.display = "block";
+  evt.currentTarget.className += " w3-dark-grey";
+}
+
+var mybtn = document.getElementsByClassName("testbtn")[0];
+mybtn.click();
