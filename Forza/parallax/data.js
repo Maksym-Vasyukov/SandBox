b963ffbada55e86099510a1e75673a5e3f902e04
Maksym Vasyukov <maksym.vasyukov@seracom.de>
2017-10-13 13:12:58 +0200

    restructured data object. feeded by count of images only

--- Forza/parallax/data.js
+++ Forza/parallax/data.js
@@ -17,39 +17,10 @@ for (var key in dataset) {
 
 // Gallery Object
 var gallery = {
-  Sommerfest_1998 : [
-    "../assets/gallery/Sommerfest_1998/sf98001.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98002.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98003.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98004.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98005.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98006.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98007.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98008.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98009.jpg",
-    "../assets/gallery/Sommerfest_1998/sf98010.jpg",
-  ],
-  Sommerfest_1999 :[
-    "../assets/gallery/Sommerfest_1999/sf99001.jpg",
-    "../assets/gallery/Sommerfest_1999/sf99002.jpg",
-    "../assets/gallery/Sommerfest_1999/sf99003.jpg",
-    "../assets/gallery/Sommerfest_1999/sf99004.jpg",
-    "../assets/gallery/Sommerfest_1999/sf99005.jpg",
-    "../assets/gallery/Sommerfest_1999/sf99006.jpg",
-    "../assets/gallery/Sommerfest_1999/sf99007.jpg",
-  ],
-  Sommerfest_2000 :[
-    "../assets/gallery/Sommerfest_2000/sf00001.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00002.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00003.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00004.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00005.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00006.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00007.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00008.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00009.jpg",
-    "../assets/gallery/Sommerfest_2000/sf00010.jpg",
-  ],
+  Sommerfest_1998: 10,
+  Sommerfest_1999: 7,
+  Sommerfest_2000: 10,
+  Sommerfest_2011: 27,
 
 };
 
@@ -71,16 +42,16 @@ var gallery = {
       if (!gallery.hasOwnProperty(key)) continue;
 
       // referenced by key of array in Object
-      var obj = gallery[key];
+      var imageCount = gallery[key];
+
       // Sets up a link
       var title = key.replace('_', ' ');
       $('#scrollmenu').append('<a link=#' + key + '>' + title + '</a>');
       // add image-container to gallery
       $('#galleryContainer').append('<div class="image-container" id=' + key + '><h2>' + title + '</h2></div>');
       // add images to container
-      for (var prop in obj) {
-        if(!obj.hasOwnProperty(prop)) continue;
-        $('#' + key).append('<div class="image-holder"><img src=' + obj[prop] + ' style="width:100%" onclick="onClick(this)" class="w3-hover-opacity gallery-image" ></div>');
+      for (var i = 1; i<= imageCount; i++) {
+        $('#' + key).append('<div class="image-holder"><img src="../assets/gallery/' + key + '/' + i + '.jpg" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity gallery-image" ></div>');
       }
       // hide the newly build container
       $('#' + key).hide();
